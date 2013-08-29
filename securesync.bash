a3ba183e78932734cd682e9f30b36050c64d6366
mschmitzz <mschmitz@riseup.net>
2013-08-28 23:07:26 -0500

    first commit

---
+++ securesync.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SOURCE=/var/backups/*.enc
+DEST=1234@usw-s123.rsync.net:
+TAR=#path to directory to be backed up#
+ 
+#Change working directory to /var/backups
+cd /var/backups
+ 
+#Create an archive of the directory to be backed up#
+tar -zcvf ##.tar $TAR
+ 
+#Encrypt the tarball#
+#will be changed to symetric keyfile and then asymentric keyfile#
+openssl enc -aes-256-cbc -salt -in ##.tar -out ##.enc -pass pass:##
+ 
+#Rsync the encrypted tarball to rsync.net#
+/usr/bin/rsync -av --delete $SOURCE $DEST
+  
+#Cleanup old archives
+rm -rf *.tar *.enc
+
+
