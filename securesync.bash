fbb29380d6de1baa93573052e9a3fb134536378d
mschmitzz <mschmitz@riseup.net>
2013-08-29 10:47:01 -0500

    Second Commit by origional author.

--- securesync.bash
+++ securesync.bash
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-SOURCE=/var/backups/*.enc
-DEST=1234@usw-s123.rsync.net:
-TAR=#path to directory to be backed up#
+if [ -r securesyncvar.bash ]; then
+ source securesyncvar.bash
+else
+ exit 1
+fi
+
+#These are being called in a different file
+#SOURCE=/var/backups/*.enc#
+#DEST=1234@usw-s123.rsync.net:#
+#TAR=#path to directory to be backed up#
  
 #Change working directory to /var/backups
 cd /var/backups
