1a1407a91529b2d1949d2aa2b5e899106a10cfe5
mschmitzz <mschmitz@riseup.net>
2013-11-23 10:06:44 -0600

    new file:   restore.bash

---
+++ restore.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#list devices 
+
+read -p "please enter the device where you would like to restore from." DEVICE
+read -p "please enter the name of the file that  you would like to decrypt." FILE
+
+
+
